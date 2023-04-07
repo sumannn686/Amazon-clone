fc38fb3ed21c4ebc0df515b6d16c16f6f4be2c8e
Suman Niroula <sumanniraula00@gmail.com>
2023-04-07 17:09:09 +0545

    header changed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./Header.css";
 import image from "../ama.png";
 import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 const Header = () => {
   return (
     <div className="header">
@@ -31,9 +32,9 @@ const Header = () => {
         <span className="header__option-LineOne">Your</span>
             <span className="header__option-LineTwo">Prime</span>
         </div>
-        <div className="header__option">
-        <span className="header__option-LineOne">Hello</span>
-            <span className="header__option-LineTwo">SignIn</span>
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__option-LineTwo header__basketCount">0</span>
         </div>
 
        </div>
