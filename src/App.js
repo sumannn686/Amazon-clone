d62fea184eeeca4828e5412f34961100cd76302d
Suman Niroula <sumanniraula00@gmail.com>
2023-04-07 16:56:08 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Components/Header'
+
+const App = () => {
+  return (
+    <div className='App'>
+    <Header  />
+    
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/Header.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Header.css";
+import image from "../ama.png";
+import SearchIcon from '@mui/icons-material/Search';
+const Header = () => {
+  return (
+    <div className="header">
+      {/*image*/}
+      <img src={image} className="header_img" alt="logo" />
+
+      {/*search bar*/}
+      
+      <div className="header__search">
+        <input className="header__search-input" type="text" />
+      <SearchIcon className="header__search-icon" />
+        
+      </div>
+
+       {/*header Nav*/}
+       <div   className="header_nav">
+
+        <div className="header__option">
+            <span className="header__option-LineOne">Hello</span>
+            <span className="header__option-LineTwo">SignIn</span>
+        </div>
+        <div className="header__option">
+        <span className="header__option-LineOne">Return</span>
+            <span className="header__option-LineTwo">& Orders</span>
+        </div>
+        <div className="header__option">
+        <span className="header__option-LineOne">Your</span>
+            <span className="header__option-LineTwo">Prime</span>
+        </div>
+        <div className="header__option">
+        <span className="header__option-LineOne">Hello</span>
+            <span className="header__option-LineTwo">SignIn</span>
+        </div>
+
+       </div>
+
+
+
+
+    </div>
+  );
+};
+
+export default Header;
